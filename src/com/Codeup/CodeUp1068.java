531a2858674c053edf639fa6f713f2e0c00861c6
JG HAN <gojkhan13@gmail.com>
2020-11-13 12:33:58 +0900

    11.13 CodeUp ~1070

---
+++ src/com/Codeup/CodeUp1068.java
@@ -0,0 +1,22 @@
+package com.Codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1068 {
+  public static void main(String[] args) {
+
+  	Scanner sc = new Scanner(System.in);
+  	
+  	int a = sc.nextInt();
+  	
+  	if(a >= 90) {
+  		System.out.println("A");
+  	}else if (a >= 70) {
+  		System.out.println("B");  		
+  	}else if (a >= 40) {
+  		System.out.println("C");  		
+  	}else if (a >= 0) {
+  		System.out.println("D");  		
+  	}  	
+  }      
+}
